ffc72bb9483c35f74e3060c740bbf9a9c2b39fee
samii89 <samii89.23@gmail.com>
2013-10-17 09:30:58 +0530

    Commit :D

---
+++ Oct17T1/Oct17T1/WordBreaker.cs
@@ -0,0 +1,34 @@
+﻿
+namespace Oct17T1
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Collections;
+
+    public class SpaceBreaker : TokenBreaker
+    {
+        public void breakIntoTokens(ArrayList tokenList)
+        {
+            for (int i = 0; i < tokenList.Count; i++ )
+            {
+                Token token = (Token)tokenList[i];
+
+                if(token.Type == TokenType.UNDEFINED)
+                    continue;
+
+                bool isEnd = false;
+
+                for (int j = 0; j < token.Value.Length ; j++ )
+                {
+                    char c = token.Value[i];
+
+                    if((c == ' ')){
+
+                    }
+                }
+            }
+        }
+    }
+}
